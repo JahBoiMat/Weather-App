bc46fd324d10bcce150fd51ea4c09aba475bafb8
JahBoiMat <matveg2911@gmail.com>
2025-03-26 10:00:41 +0100

    v0.0.3
    Added map

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
-import json
 import tkinter as tk
 from tkinter import messagebox
+import tkintermapview as tkmap
 import time as t
 import os
 from dotenv import load_dotenv
@@ -10,5 +10,17 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 YOUR_API_KEY = os.getenv('apikey')
 
-print("<<-- Weather App Console -->>     Version: 0.0.2")
+print("<<-- Weather App Console -->>     Version: 0.0.3")
+
+# create tkinter window
+root_tk = tk.Tk()
+root_tk.geometry(f"{800}x{600}")
+root_tk.title("Dr.Matvey's Syke Værapp")
+
+# create map widget
+map_widget = tkmap.TkinterMapView(root_tk, width=800, height=600, corner_radius=0)
+map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+root_tk.mainloop()
+
 
