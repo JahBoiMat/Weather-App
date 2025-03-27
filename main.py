57558926ef6258002191c4462f3f6bee1032edb0
JahBoiMat <matveg2911@gmail.com>
2025-03-27 09:13:28 +0100

    v0.0.7
    Get Forecast data successful
    
    Other minor changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 print("")
-print("<<-- Dr.Matvey's Weather App Console Log-->>     Version: 0.0.6")
+print("<<-- Dr.Matvey's Weather App Console Log-->>     Version: 0.0.7")
 print("""
 Log Actor List: 
 [ Ack ] (Acknowledge) = Api acknowledge message
@@ -15,14 +15,14 @@ Log Actor List:
 [ Err ] (Error) = Error/Exeption - Related to current process
 [ Idl ] (Idle) = Idle/Waiting, no processes
 [=STR=] (Start) = Marks start of a process - The following logs are related to this process until next [=STR=] is called
-This console is used for debugging only, please look to TkInter Window
+This console is used for debugging only, please look to TkInter Window for weather data
 """)
 print("<<===== LOG =====>>")
-print("[=STR=] - Main Program Start | Retrieve API key ")
+print("[=STR=] - Main Start")
 load_dotenv(".env")
-YOUR_API_KEY = os.getenv('apikey')
+YOUR_API_KEY = os.getenv('apikey') #testkey = test key (t) apikey = main key
 set_marker = None
-print("[ Idl ] - Idle")
+print("[ Idl ] - Idle, Click on map") 
 
 # - - - - LOGIC - - - -
 
@@ -41,8 +41,11 @@ def getweathercurrent(lat, lon):
         city = data["name"]
         condition = data["weather"][0]["description"]
         temperature = data["main"]["temp"]
+        min_temp = data["main"]["temp_min"]
+        max_temp = data["main"]["temp_max"]
+        pressure = data["main"]["pressure"]
         humidity = data["main"]["humidity"]
-        print("[ Sys ] - Readable Format: ","City: ", city ,"Country: ", country,"Condition: ", condition,"Temperature: ", temperature,"Humidity: ", humidity)
+        print("[ Sys ] - Readable Format: ","City: ", city ,"Country: ", country,"Condition: ", condition,"Temperature: ", temperature,"Minimum Temp: ", min_temp,"Maximum Temp: ", max_temp,"Humidity: ", humidity,"Pressure: ", pressure)
         
     elif api_response.status_code==404:
         print("[ Err ] Denied - Status: ", api_response.status_code)
@@ -57,19 +60,14 @@ def getweatherforecast(lat, lon):
     print("[=STR=] - Get Weather Forecast Data")
     print("[ Sys ] - Current Function: getweatherforecast")
     print("[ Sys ] - Api call Start")
-    api_response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={YOUR_API_KEY}&units=metric")
+    api_response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=7&appid={YOUR_API_KEY}")
     data = api_response.json()
     if api_response.status_code==200:
         print("[ Ack ] - Api Status: ", api_response.status_code)
         print("[ Ack ] - Api Returned Data: ", data)
         print("[ Sys ] - Assigning Variables")
 
-        country = data["sys"]["country"]
-        city = data["name"]
-        condition = data["weather"][0]["description"]
-        temperature = data["main"]["temp"]
-        humidity = data["main"]["humidity"]
-        print("[ Sys ] - Readable Format: ","City: ", city ,"Country: ", country,"Condition: ", condition,"Temperature: ", temperature,"Humidity: ", humidity)
+        print("[ Sys ] - Rqst empty, for now")
         
     elif api_response.status_code==404:
         print("[ Err ] Denied - Status: ", api_response.status_code)
@@ -113,6 +111,7 @@ map_widget.add_left_click_map_command(place_marker)
 
 
 root_tk.mainloop()
+
 
 #KI BRUKT TIL: 
 #Slette gammel map marker slik at det ikke blir laget flere markers
