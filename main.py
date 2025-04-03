6f4666692b59005c794d1094bee21c02f912cffc
JahBoiMat <matveg2911@gmail.com>
2025-04-03 09:01:07 +0200

    v0.1.7
    Now forecast works :D
    
    Fixed typos

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import tkinter as tk
 from tkinter import messagebox
 import tkintermapview as tkmap
 import os # You dont need this if you hardcode your API key
-from dotenv import load_dotenv # You dont need this if you hardcode your API key
+from dotenv import load_dotenv # You also dont need this if you hardcode your API key
 from datetime import datetime
 
-v="0.1.6" # version number
+v="0.1.7" # version number
 print("")
 print(f"<<-- Dr.Matvey's Wicked Weather App Console Log-->>     Version: {v}")
 print("""
@@ -51,7 +51,7 @@ def getweathercurrent(lat, lon): # get current weather
 
         print(f"[ Sys ] - Readable Format: City: {city} | Country: {country} | Conditions: {condition} | Temperature: {temp}°C | Feels like: {temp_feels}°C | Pressure: {pressure}Pa | Humidity: {humidity}% | Wind Speed {wind_speed}m/s | Wind Deg: {wind_deg}°")
         weather_location.set(f"Conditions in {city}, {country}")
-        weather_current.set(f"Current condition(s): {condition}. Temperature is {temp}°C (Feels like: {temp_feels}°C) Winds at {wind_speed}m/s from {wind_deg}°. Pressure: {pressure}Pa Humidity: {humidity}%")
+        weather_current.set(f"Current condition(s): {condition.capitalize()}. Temperature is {temp}°C (Feels like: {temp_feels}°C) Winds at {wind_speed}m/s from {wind_deg}°. Pressure: {pressure}Pa Humidity: {humidity}%")
 
         print("[=END=] - Get Current Weather Data")
 
@@ -74,27 +74,24 @@ def getweatherforecast(lat, lon): # get 7 day forecast
     print("[=STR=] - Get Weather Forecast Data")
     print("[ Sys ] - Current Function: getweatherforecast")
     print("[ Sys ] - Api call Start")
-    api_response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=7&appid={YOUR_API_KEY}") #replace YOUR_API_KEY with your key to get weather data
+    api_response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=8&appid={YOUR_API_KEY}&units=metric") #replace YOUR_API_KEY with your key to get weather data
     data = api_response.json()
     if api_response.status_code==200:
         print("[ Ack ] - Api Status: ", api_response.status_code)
         print("[ Ack ] - Api Returned Data: ", data)
         
-        forecast_list = [] #Array for 7 days of data :))
-
-        for day in data["list"]:
-            print("[ Sys ] - Looping Through Days")
-            day = datetime.fromtimestamp(day["dt"]).strftime("%A %d.%m")
-            print("[ Sys ] - Assigning Variables")
+        forecast_list = []
+        print("[ Sys ] -  Adding Forecast Data to List")
+        for day in data["list"][1:]: 
+            weekday = datetime.fromtimestamp(day["dt"]).strftime("%A")
 
             condition = day["weather"][0]["description"]
-            temp = day["temp"]["day"]
             temp_min = day["temp"]["min"]
             temp_max = day["temp"]["max"]
             humidity = day["humidity"]
 
-            forecast_list.append(f"{day}: Will be {condition}. Temp - Hi:{temp_max}°C Lo:{temp_min}°C Humidity {humidity}% \n")
-            print(f"[ Sys ] - Added {day} data to list")
+            forecast_list.append(f"{weekday}: {condition.capitalize()}. Temp - Hi:{temp_max}°C Lo:{temp_min}°C Humidity: {humidity}%rh")
+            print(f"[ Sys ] - Added {weekday} data to list")
         
         print("[ Sys ] - Forecast List Updated")
         weather_forecast.set("\n".join(forecast_list))
@@ -141,8 +138,8 @@ root_tk.geometry(f"{800}x{500}")
 root_tk.title("Dr. Matvey's Wicked Weather App")
 root_tk.iconbitmap("logo.ico") #uncomment to add logo
 root_tk.resizable(False, False)
-root_tk.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
-root_tk.grid_columnconfigure((0, 1, 2, 3, 4), weight=1)
+root_tk.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
+root_tk.grid_columnconfigure((0, 1, 2, 3, 4, 5), weight=1)
 
 map_widget = tkmap.TkinterMapView(root_tk, width=500, height=500, corner_radius=0)
 map_widget.grid(row=0, column=0, rowspan=5, padx=0, pady=0)
@@ -167,82 +164,30 @@ weather_forecast = tk.StringVar()
 if set_location is None:
     weather_location.set("No location Selected")
     weather_current.set("No weather data to display. Please select a location.")
-    weather_forecast.set("No forecast data availbale. Please select a location.")
+    weather_forecast.set("No forecast data available. Please select a location")
     print("[ Sys ] - No location selected")
     print("[ Idl ] - Idle, awaiting user interaction")
 else:
     pass
 
 #Label Design / Layout
-title_static_label = tk.Label(
-    root_tk,
-    textvariable=title_static,
-    anchor=tk.CENTER,
-    font=("Arial", 14, "bold"),
-    fg="black",
-    padx=25,
-    justify=tk.CENTER,
-)
+title_static_label = tk.Label(root_tk, textvariable=title_static, anchor=tk.CENTER, font=("Arial", 14, "bold"), fg="black",padx=25, justify=tk.CENTER)
 title_static_label.grid(row=0, column=1, columnspan=2, rowspan=1)
 
-description_static_label = tk.Label(
-    root_tk,
-    textvariable=description_static,
-    anchor=tk.CENTER,
-    font=("Arial", 10),
-    fg="black",
-    padx=25,
-    justify=tk.CENTER,
-)
+description_static_label = tk.Label(root_tk, textvariable=description_static, anchor=tk.CENTER, font=("Arial", 10), fg="black", padx=25, justify=tk.CENTER)
 description_static_label.grid(row=0, column=1, columnspan=2, rowspan=2)
  
-version_label = tk.Label(
-    root_tk,
-    textvariable=version,
-    anchor=tk.CENTER,
-    font=("Arial", 6, "bold"),
-    fg="grey",
-    padx=10,
-    justify=tk.CENTER,
-)
+version_label = tk.Label(root_tk, textvariable=version, anchor=tk.CENTER, font=("Arial", 6, "bold"), fg="grey", padx=10, justify=tk.CENTER)
 version_label.grid(row=4, column=1, columnspan=2, rowspan=2)
 
-weather_location_label = tk.Label(
-    root_tk,
-    textvariable=weather_location,
-    anchor=tk.CENTER,
-    font=("Arial", 14, "bold"),
-    fg="black",
-    padx=10,
-    justify=tk.CENTER,
-)
+weather_location_label = tk.Label(root_tk, textvariable=weather_location, anchor=tk.CENTER, font=("Arial", 14, "bold"), fg="black", padx=10, justify=tk.CENTER)
 weather_location_label.grid(row=1, column=1, columnspan=2, rowspan=1)
 
-weather_current_label = tk.Label(
-    root_tk,
-    textvariable=weather_current,
-    anchor=tk.CENTER,
-    font=("Arial", 8),
-    fg="black",
-    padx=10,
-    pady=10,
-    justify=tk.CENTER,
-    wraplength=310
-)
+weather_current_label = tk.Label(root_tk, textvariable=weather_current, anchor=tk.CENTER, font=("Arial", 10), fg="black", padx=10, pady=10, justify=tk.CENTER, wraplength=310)
 weather_current_label.grid(row=1, column=1, columnspan=2, rowspan=2)
 
-weather_forecast_label = tk.Label(
-    root_tk,
-    textvariable=weather_forecast,
-    anchor=tk.CENTER,
-    font=("Arial", 8),
-    fg="black",
-    padx=10,
-    pady=10,
-    justify=tk.CENTER,
-    wraplength=320
-)
-weather_forecast_label.grid(row=1, column=1, columnspan=2, rowspan=3)
+weather_forecast_label = tk.Label(root_tk, textvariable=weather_forecast, anchor=tk.CENTER, font=("Arial", 8), fg="black", padx=10, pady=10, justify=tk.CENTER, wraplength=320)
+weather_forecast_label.grid(row=2, column=1, columnspan=2, rowspan=4)
 
 
 
